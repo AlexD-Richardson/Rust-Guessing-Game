a496b8dab9604fe8b8b55abc57eaf3cf01cae432
Alex Richardson <richardson.d.alex@gmail.com>
2018-12-16 12:17:19 -0600

    Finished guessing game for now

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,33 @@ use rand::Rng;
 
 fn main() {
 
-    let secret_number = rand::thread_rng().gen_range(1, 10);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("Guess the number!");
+    println!("Guess a number between 1 and 100!");
 
-    println!("The secret number is: {}", secret_number);
+    loop {
+        println!("Please input your guess.");
 
-    println!("Please input your guess.");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
